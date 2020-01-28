72eab9cf0488fa34b6fb3b400ec8dd1ac64b85a8
Vibhor Agarwal <agarwalvibhor99@gmail.com>
2020-01-28 09:52:12 -0500

    initial commit
    Took help from the textbook to write the static functions from macros and malloc, free, calloc

--- mm.c
+++ mm.c
@@ -1,7 +1,7 @@
 /*
  * mm.c
  *
- * Name: [FILL IN]
+ * Name: Vibhor Agarwal
  *
  * NOTE TO STUDENTS: Replace this header comment with your own header
  * comment that gives a high level description of your solution.
@@ -50,6 +50,150 @@
 /* What is the correct alignment? */
 #define ALIGNMENT 16
 
+/* Basic constants */
+
+#define WSIZE       4       /* Word and header/footer size(bytes) */
+#define DSIZE       8       /* Double word size (bytes) */
+#define CHUNKSIZE   (1<<12) /*Extended heap by this amount (bytes) */ 
+/* Static Inline Functions */
+
+static size_t max(size_t x, size_t y){
+    return ((x) > (y)? (x) : (y));
+}
+
+static size_t pack(int size, int alloc){
+    return ((size) | (alloc));
+}
+
+static unsigned size_t get(const void* p){
+   return (*(unsigned int *)(p));
+}
+
+static void put(const void* p, int val){
+    return (*(unsigned int *)(p) = (val));
+}
+
+static size_t get_size(const void* p){
+  return (get(p) & ~0x7); 
+}
+
+static size_t get_alloc(const void* p){
+    return 8(get(p) & 0x1);
+}
+
+static void* HDRP(void* bp){
+    ((char *)(bp) - WSIZE);
+}
+
+static void FTRP(void* bp){
+    ((char *)(bp) + get_size(HDRP(bp)) - DSIZE)
+}
+
+static void NEXT_BKLP(void* bp){
+    ((char *)(bp) + get_size(((char *)(bp) - WSIZE)));
+}
+
+static void PREV_BLKP(void* bp){
+    ((char *)(bp) - get_size(((char *)(bp) - DSIZE)));
+}
+
+/*
+ * Additional Functions
+ */
+
+/*
+* coalesce
+*/
+
+static void *coalesce(void *bp){
+    size_t prev_alloc = get_alloc(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = get_alloc(HDRP(NEXT_BKLP(bp)));
+    size_t size = get_size(HDRP(bp));
+
+    if (prev_alloc && next_alloc){  /* Case 1 */
+        return bp;
+    }
+
+    else if (prev_alloc && !next_alloc){    /* Case 2 */
+        size += get_size(HDRP(NEXT_BKLP(bp)));
+        put(HDRP(bp), pack(size, 0));
+        put(FTRP(bp), pack(size, 0));
+    }
+
+    else if (!prev_alloc && next_alloc){    /* Case 3 */
+    size += get_size(HDRP(PREV_BLKP(bp)));
+    put(FTRP(bp), pack(size, 0));
+    put(HDRP(PREV_BLKP(bp)), pack(size, 0));
+    bp = PREV_BLKP(bp);
+    }
+
+    else{
+        size += get_size(HDRP(PREV_BLKP(bp))) + get_size(FTRP(NEXT_BKLP(bp)));
+        put(HDRP(PREV_BLKP(bp)), pack(size, 0));
+        put(FTRP(NEXT_BKLP(bp)), pack(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    return bp;
+}
+
+/*
+ * extended_heap
+ */
+
+static void *extended_heap(size_t words){
+    char *bp;
+    size_t size;
+
+    /* Allocate an even number of words to maintain alignment */
+    size = (words % 2) ? (words+1) * WSIZE : words+ WSKZE;
+    if ((long)(bp = mem_sbrk(size)) == -1)
+        return NULL;
+
+    /* Initializew free block header/footer and the epologue header */
+    put(HDRP(bp), pack(size, 0));       /* Free block header */
+    put(FTRP(bp), pack(size, 0));       /* Free block footer */
+    put(HDRP(NEXT_BKLP(bp)), pack(0,1));/* New epilogue header */
+
+    /* Coalesce if the previous block was free */
+    return coalesce(bp);
+}
+
+/*
+ * find_fits
+ */
+static void *find_fits(size_t asize){
+    /* First-fit search */
+    void *bp;
+    for(bp = heap_listp; get_size(HDRP(bp))>0; bp = NEXT_BKLP(bp)){
+        if (!get_alloc(HDRP(bp)) && (asize <= get_size(HDRP(bp)))){
+            return bp;
+        }
+    }
+    return NULL; /* No fit */
+//#endif 
+}
+
+/*
+ * place
+ */
+static void place(void *bp, size_t asize){
+    size_t csize = get_size(HDRP(bp));
+
+    if((csize - asize) >= (2*DSIZE)){
+        put(HDRP(bp), pack(asize, 1));
+        put(FTRP(bp), pack(asize, 1));
+        bp = NEXT_BKLP(bp);
+        put(HDRP(bp), pack(csize-asize, 0));
+        put(FTRP(bp), pack(csize-asize), 0);
+    }
+    else{
+        put(HDRP(bp), pack(csize, 1));
+        put(FTRP(bp), pack(csize, 1));
+    }
+}
+
+
+
 /* rounds up to the nearest multiple of ALIGNMENT */
 static size_t align(size_t x)
 {
@@ -62,7 +206,17 @@ static size_t align(size_t x)
 bool mm_init(void)
 {
     /* IMPLEMENT THIS */
-    return true;
+    if((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1)
+        return -1;
+    put(heap_listp, 0);
+    put(heap_listp + (1*WSIZE), pack(DSIZE, 1));
+    put(heap_listp + (2*WSIZE), pack(DSIZE, 1));
+    put(heap_listp + (3*WSIZE), pack(0,1));
+    heap_listp += (2*WSIZE);
+
+    if(extended_heap(CHUNKSIZE/WSIZE) == NULL)
+        return -1;
+    return 0;
 }
 
 /*
@@ -71,7 +225,25 @@ bool mm_init(void)
 void* malloc(size_t size)
 {
     /* IMPLEMENT THIS */
-    return NULL;
+    size_t asize;
+    size_t extendsize;
+    void *bp;
+
+
+    if(size == 0)
+        return NULL;
+
+
+    if(size <= DSIZE)
+        asize = 2*DSIZE;
+    else
+        asize = DSIZE * ((size + (DSIZE) + (DSIZE-1)) / DSIZE);
+
+
+    if((bp = find_fit(asize)) != NULL){
+        place(bp, asize);
+        return bp;
+    }
 }
 
 /*
@@ -80,7 +252,11 @@ void* malloc(size_t size)
 void free(void* ptr)
 {
     /* IMPLEMENT THIS */
-    return;
+    size_t size = get_size(HDRP(ptr));
+
+    put(HDRP(ptr), pack(size, 0));
+    put(FTRP(bp), pack(size,0));
+    coalesce(bp);
 }
 
 /*
@@ -106,6 +282,8 @@ void* calloc(size_t nmemb, size_t size)
     }
     return ptr;
 }
+
+
 
 /*
  * Returns whether the pointer is in the heap.
